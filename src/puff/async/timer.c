798938875c78ab47db4229b432f6ff6721a2aafc
PABOIS Gaël <gael.pabois@developpement-durable.gouv.fr>
2023-09-06 18:29:07 +0200

    up

--- src/puff/async/timer.c
+++ src/puff/async/timer.c
@@ -1,12 +1,10 @@
 #include <puff/async/timer.h>
 #include <puff/async/loop.h>
 
-async_def(char, async_sleep, int, ms) {
-    AsyncTimer_t timer = {
-        .coro = current_coro,
-        .t = clock() + ms * 1000 / CLOCKS_PER_SEC
-    };
-    
-    yield(async_timer(&timer));
-    return 1;
+#define async_sleep(ms) {\
+    AsyncTimer_t timer = {\
+        .coro = current_coro,\
+        .t = clock() + ms * 1000 / CLOCKS_PER_SEC\
+    };\    
+    yield(async_timer(&timer));\
 }
